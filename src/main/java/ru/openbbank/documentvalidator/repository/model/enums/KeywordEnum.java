6eda0a850b2841091e66fc8944fe7b0730ca602e
vuvarov <vova.uvarov@gmail.com>
2020-08-30 11:05:22 +0300

    update documentType and keywordEnum

--- src/main/java/ru/openbbank/documentvalidator/repository/model/enums/KeywordEnum.java
+++ src/main/java/ru/openbbank/documentvalidator/repository/model/enums/KeywordEnum.java
@@ -56,18 +56,38 @@ public enum KeywordEnum {
     REFERENCE_DUPLICATE("referenceDuplicate"),
     SIZE("size"),
     EQUAL("equal"),
+    EQUAL2("equal2"),
+    EQUAL3("equal3"),
     NOT_EQUAL("notEqual"),
+    CHECK_UIN_KEY("checkUinKey"),
     CHECK_ACCOUNT("checkAccount"),
     CHECK_ACCOUNT_KEY("checkAccountKey"),
+    CHECK_ACCOUNT_KEY_2("checkAccountKey2"),
     CHECK_CURRENCY_AMOUNT("checkCurrencyAmount"),
     CHECK_CURRENCY_AMOUNT2("checkCurrencyAmount2"),
     ARRAY_REQUIRED_IF("arrayRequiredIf"),
     PRECISION("precision"),
     UNIQUE_DOCUMENT("uniqueDocument"),
+    JAVA_IMPLEMENTATION("java"),
+    JAVA_IMPLEMENTATION_2("java2"),
+    JAVA_IMPLEMENTATION_3("java3"),
+    JAVA_IMPLEMENTATION_4("java4"),
+    JAVA_IMPLEMENTATION_5("java5"),
+    JAVA_IMPLEMENTATION_6("java6"),
+    JAVA_IMPLEMENTATION_7("java7"),
+    JAVA_IMPLEMENTATION_8("java8"),
+    JAVA_IMPLEMENTATION_9("java9"),
+    JAVA_IMPLEMENTATION_10("java10"),
     EXPRESSION("expression"),
     EXPRESSION_2("expression2"),
     EXPRESSION_3("expression3"),
+    EXPRESSION_4("expression4"),
+    EXPRESSION_5("expression5"),
+    EXPRESSION_6("expression6"),
+    EXPRESSION_7("expression7"),
+    EXPRESSION_8("expression8"),
     FILE_EXTENSION("fileExtension"),
+    FILE_NAME("fileName"),
     ARRAY_SIZE("arraySize"),
     SUM_ARRAY_ELEMENTS_EQUAL("sumArrayElementsEqual"),
 
@@ -79,6 +99,12 @@ public enum KeywordEnum {
     CONDITION_2("condition2"),
     CONDITION_3("condition3"),
     CONDITION_4("condition4"),
+    CONDITION_5("condition5"),
+    CONDITION_6("condition6"),
+    CONDITION_7("condition7"),
+    CONDITION_8("condition8"),
+    CONDITION_9("condition9"),
+    CONDITION_10("condition10"),
     ALWAYS_FAIL("alwaysFail"),
     SUBSTRING("substring"),
     SUBSTRING_2("substring2"),
@@ -90,7 +116,7 @@ public enum KeywordEnum {
 
     public static KeywordEnum of(String source) {
         return Arrays.stream(values())
-                .filter(v -> Objects.equals(v.value, source.replaceAll("\\[\\d+]","")))
+                .filter(v -> Objects.equals(v.value, source.replaceAll("\\[\\d+]", "")))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("unknown value " + source));
     }
